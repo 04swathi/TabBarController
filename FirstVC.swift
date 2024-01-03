971bfcebebc2cb8fdf82117f05e398dd32a2449b
Swathi k <111586588+04swathi@users.noreply.github.com>
2024-01-03 12:40:51 +0530

    Tab Bar Controller

---
+++ FirstVC.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    @IBOutlet weak var FirstVC: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ SecondVC.swift
@@ -0,0 +1,12 @@
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    @IBOutlet weak var SecondVC: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
